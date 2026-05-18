e7ad8ddd71c4159e723739c4ed432b607f8d5b6f
Ashmita katwal <katwalasmita0@gmail.com>
2026-05-18 09:19:24 +0545

    Something new flow

--- push.ps1
+++ push.ps1
@@ -5,7 +5,7 @@ git commit -m "$commitMessage"
 # Push the commit to the remote repository whatever  it is
 $remoteName = git remote | Select-Object -First 1
 if ($remoteName) {
-    git push $remoteName
+    git push orign $remoteName
 } else {
     Write-Host "No remote repository found. Please set up a remote repository to push your commits."
 }
